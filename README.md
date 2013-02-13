be1d91fc724e69963627ba0b3a974e2b10defb8f
John Devlin <j@johndevlin.me>
2013-02-13 00:02:50 +0000

    Adding LocalStorage values to more DOM elements.

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 Cal Counter
 ===========
 
-The fuck?
----------
-
 Just a wee calorie counter I'm making with HTML5 [local storage](http://www.w3schools.com/html/html5_webstorage.asp). 
--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,24 @@ if (Modernizr.localstorage) {
 	// maybe try dojox.storage or a third-party solution
 }
 
+// Displays LocalStorage in Input fields & in sidebar
 $(document).ready(function () {
 	$('input#breakfast').val(localStorage["breakfast"]);
+	$('h2#breakfast-total span').append(localStorage["breakfast"]);
+	
 	$('input#lunch').val(localStorage["lunch"]);
+	$('h2#lunch-total span').append(localStorage["lunch"]);
+	
+	$('input#dinner').val(localStorage["dinner"]);
+	$('h2#dinner-total span').append(localStorage["dinner"]);
 });
 
+// Saves Input data (from input.stored) as LocalStorage
 $('.stored').keyup(function () {
-    localStorage[$(this).attr('name')] = $(this).val();
+    localStorage[$(this).attr('id')] = $(this).val();
 });
 
+// Displays LocalStorage on Click
 $('p#display-breakfast').click(function() {
 	alert( "I munched " + localStorage.getItem('breakfast') + " calories for breakfast.");
 });
@@ -22,9 +31,13 @@ $('p#display-lunch').click(function() {
 	alert( "I munched " + localStorage.getItem('lunch') + " calories for lunch.");
 });
 
+$('p#display-dinner').click(function() {
+	alert( "I munched " + localStorage.getItem('dinner') + " calories for dinner.");
+});
 
-// Clear data
+// Clears data and Input values
 $('p#clear').click(function() {
     localStorage.clear();
+	$("input").val('');
 });
 
