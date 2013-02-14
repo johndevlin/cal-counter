8c9fb8b6138c2235d3c604f7a92211402921b110
John Devlin <j@johndevlin.me>
2013-02-14 00:07:29 +0000

    Still buggy as hell.

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,16 @@ if (Modernizr.localstorage) {
 	// maybe try dojox.storage or a third-party solution
 }
 
-$(document).ready(function() {
-	$('h1 span').empty().append(total);
-});
-
-$('p#go').click(function() {
-    localStorage[$('select option:selected').attr('value')] = $('input.store').val();
-	$('h1 span').empty().append(total);
-});
+// $(document).ready(function() {
+// 	// Set the total in a H1's span onload
+// 	$('h1 span').empty().append(total);
+// 	
+// 	// Set totals in a table onload
+// 	$('td#breakfast-total').empty().append(breakfast);
+// 	$('td#lunch-total').empty().append(lunch);
+// 	$('td#dinner-total').empty().append(dinner);
+// 	$('td#snacks-total').empty().append(snacks);
+// });
 
 var breakfast = localStorage["breakfast"];
 var lunch = localStorage["lunch"];
@@ -21,13 +23,19 @@ var snacks = localStorage["snacks"];
 
 var total = Number(breakfast) + Number(lunch) + Number(dinner) + Number(snacks);
 
+$('p#go').click(function() {
+    localStorage[$('select option:selected').attr('value')] = $('input.store').val();
+
+	$('h1 span').empty().append(total);
+});
+
 // Clears data and Input values
 $('p#reset').click(function() {
     localStorage.clear();
-	localStorage.setItem( 'breakfast', 0)
-	localStorage.setItem( 'lunch', 0)
-	localStorage.setItem( 'dinner', 0)
-	localStorage.setItem( 'snacks', 0)
+	localStorage.setItem('breakfast', 0);
+	localStorage.setItem('lunch', 0);
+	localStorage.setItem('dinner', 0);
+	localStorage.setItem('snacks', 0);
 	$("input").val('');
-	$('h1 span').empty().append(total);
+	$('h1 span').empty().append(total);	
 });
