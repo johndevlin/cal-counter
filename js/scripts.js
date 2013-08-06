e65cd8af6b8098190657c3b173023033f63ceba5
johndevlin <john@mammoth.tv>
2013-08-06 16:30:24 +0100

    Added store.js to the repo.

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,11 @@ if (Modernizr.localstorage) {
 	// maybe try dojox.storage or a third-party solution
 }
 
-// $(document).ready(function() {
-// 	// Set the total in a H1's span onload
-// 	$('h1 span').empty().append(total);
-// 	
-// 	// Set totals in a table onload
-// 	$('td#breakfast-total').empty().append(breakfast);
-// 	$('td#lunch-total').empty().append(lunch);
-// 	$('td#dinner-total').empty().append(dinner);
-// 	$('td#snacks-total').empty().append(snacks);
-// });
+
+
+
+
+// Localstorage variables 
 
 var breakfast = localStorage["breakfast"];
 var lunch = localStorage["lunch"];
@@ -23,19 +18,59 @@ var snacks = localStorage["snacks"];
 
 var total = Number(breakfast) + Number(lunch) + Number(dinner) + Number(snacks);
 
-$('p#go').click(function() {
-    localStorage[$('select option:selected').attr('value')] = $('input.store').val();
 
+
+
+
+// Printing localstorage date to table
+
+$(document).ready(function() {
+	// Set the total in a H1's span onload
 	$('h1 span').empty().append(total);
+	
+	// Set totals in a table onload
+	$('td#breakfast-total').empty().append(breakfast);
+	$('td#lunch-total').empty().append(lunch);
+	$('td#dinner-total').empty().append(dinner);
+	$('td#snacks-total').empty().append(snacks);
 });
 
-// Clears data and Input values
+
+
+
+
+// Capturing date
+
+$('p#go').click(function() {
+	
+	// Gets data from input
+    localStorage[$('select option:selected').attr('value')] = $('input.store').val();
+	
+	// Prints data to H1
+	$('h1 span').empty().append(total);
+	
+	// Reload page
+	location.reload();
+	
+});
+
+
+
+
+
+// Clears all data and table values
+
 $('p#reset').click(function() {
-    localStorage.clear();
+	
+    store.clear()
+
 	localStorage.setItem('breakfast', 0);
 	localStorage.setItem('lunch', 0);
 	localStorage.setItem('dinner', 0);
 	localStorage.setItem('snacks', 0);
+	
 	$("input").val('');
 	$('h1 span').empty().append(total);	
+	
+	location.reload();
 });
